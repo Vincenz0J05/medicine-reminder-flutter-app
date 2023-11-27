b088f9701836070d6ebb09cb0222acdfb1c10e5a
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2023-11-27 23:13:29 +0100

    added firebase and dateselector

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:intl/intl.dart';
+import 'package:medication_reminder_app/widgets/date_selector.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,59 +15,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter firebase demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter firebase projet'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      title: 'Flutter Demo',
+      home: Scaffold(
+        body: DateSelector(
+          formattedDate: DateFormat('dd MMMM').format(DateTime.now()),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
---
+++ lib/widgets/date_selector.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DateSelector extends StatefulWidget {
+  final String formattedDate;
+
+  const DateSelector({
+    Key? key,
+    required this.formattedDate,
+  }) : super(key: key);
+
+  @override
+  DateSelectorState createState() => DateSelectorState();
+}
+
+class DateSelectorState extends State<DateSelector> {
+  bool _isExpanded = true;
+  DateTime _selectedDate = DateTime.now();
+
+  void _onDateTap(DateTime date) {
+    setState(() {
+      _selectedDate = date;
+    });
+  }
+
+  Widget _buildDateTile(DateTime date, bool isSelected) {
+    final dateNumber = DateFormat('d').format(date);
+    final day = DateFormat('EEE').format(date).substring(0, 3);
+
+    return GestureDetector(
+      onTap: () => _onDateTap(date),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 5),
+        width: 60,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: isSelected ? const Color(0xffeb6081) : const Color(0xFFeff0f4),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                dateNumber,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: isSelected ? Colors.white : Colors.black,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                day,
+                style: TextStyle(
+                  color: isSelected ? Colors.white : Colors.black,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 20),
+        Theme(
+            data: ThemeData(dividerColor: Colors.transparent),
+            child: Material(
+              color: Colors
+                  .transparent, // Set the color of Material to transparent
+              child: ExpansionTile(
+                onExpansionChanged: (bool expanded) {
+                  setState(() {
+                    _isExpanded = expanded;
+                  });
+                },
+                initiallyExpanded: _isExpanded,
+                trailing: RotatedBox(
+                  quarterTurns: _isExpanded ? 0 : 2,
+                  child: const Icon(
+                    Icons.expand_more,
+                    color: Color(0xffeb6081),
+                  ),
+                ),
+                title: Text(
+                  'Today, ${widget.formattedDate}',
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                    color: Colors.black,
+                  ),
+                ),
+                children: [
+                  SizedBox(
+                    height: 90,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 7,
+                      itemBuilder: (BuildContext context, int index) {
+                        DateTime date =
+                            DateTime.now().add(Duration(days: index));
+                        return _buildDateTile(
+                            date, date.isSameDay(_selectedDate));
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ))
+      ],
+    );
+  }
+}
+
+extension DateTimeExtension on DateTime {
+  bool isSameDay(DateTime other) {
+    return year == other.year && month == other.month && day == other.day;
+  }
+}
