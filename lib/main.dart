27a73275074be8f1e1303859ef86c2f5462b6f82
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2023-11-29 13:23:55 +0100

    reformatted main.dart to only include homescreen class

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:intl/intl.dart';
-import 'package:medication_reminder_app/widgets/date_selector.dart';
+import 'package:medication_reminder_app/screens/home_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,12 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        body: DateSelector(
-          formattedDate: DateFormat('dd MMMM').format(DateTime.now()),
-        ),
-      ),
+      home: HomeScreen(), 
     );
   }
 }
