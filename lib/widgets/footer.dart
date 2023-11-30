824e72812faa4dd10d4015f6f7318df84318eb57
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2023-11-30 10:05:45 +0100

    added the sheet to appear whenever the plus icon is clicked

--- lib/widgets/footer.dart
+++ lib/widgets/footer.dart
@@ -1,33 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:medication_reminder_app/screens/create_medication_screen.dart';
 
-class Footer extends StatelessWidget {
-  const Footer({Key? key}) : super(key: key);
+class Footer extends StatefulWidget {
+  final VoidCallback onButtonPressed;
 
+  const Footer({Key? key, required this.onButtonPressed}) : super(key: key);
+
+  @override
+  State<Footer> createState() => _FooterState();
+}
+
+class _FooterState extends State<Footer> {
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.only(bottom: 15),
-      child: const Row(
-        mainAxisAlignment:
-            MainAxisAlignment.spaceEvenly, // Ensures even spacing
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Expanded(child: Icon(Icons.home, size: 30)),
+          const Expanded(child: Icon(Icons.home, size: 30)),
           FloatingActionButton(
             backgroundColor: const Color(0xffeb6081),
-            onPressed: () => _onButtonPressed(context),
-            elevation: 0, // Remove the default shadow
+            onPressed: () => widget.onButtonPressed(),
+            elevation: 0,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(30.0),
             ),
             mini: false,
-            child: Icon(Icons.add, color: Colors.white),
+            child: const Icon(Icons.add, color: Colors.white),
           ),
-          Expanded(child: Icon(Icons.history, size: 30)),
+          const Expanded(child: Icon(Icons.history, size: 30)),
         ],
       ),
     );
   }
-
-  
 }
