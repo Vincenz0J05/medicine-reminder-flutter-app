ecf84846b9b704d23e3cb9348f17e45b8a74394f
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2023-12-03 22:57:01 +0100

    added controller parameter, onchanged optional

--- lib/widgets/input_style.dart
+++ lib/widgets/input_style.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 
 Widget inputStyle({
   IconData? prefixIcon,
-  String? hintText,
+  String? hintText, 
+  void Function(dynamic value)? onChanged, // Make onChanged optional
+  required TextEditingController controller, // Make controller required
 }) {
   return TextField(
+    onChanged: onChanged, // Assign onChanged callback if provided
+    controller: controller, // Assign controller
     decoration: InputDecoration(
       filled: true,
       border: OutlineInputBorder(
