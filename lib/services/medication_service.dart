eb29caec55785155cbdce6efed17cd60113956f9
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2023-12-03 22:55:45 +0100

    added create and read operations

--- lib/services/medication_service.dart
+++ lib/services/medication_service.dart
@@ -1,21 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/foundation.dart';
 import '../models/medicine.dart';
 
 class MedicationService {
-  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  final CollectionReference medicineCollection =
+      FirebaseFirestore.instance.collection('medicine');
 
-  Future<void> addMedication(Medicine medicine) async {
-    // Assuming you want to generate a new ID if none is provided:
-    DocumentReference documentReferencer = 
-        medicine.id.isNotEmpty 
-            ? firestore.collection('medications').doc(medicine.id)
-            : firestore.collection('medications').doc(); // Firestore generates the ID automatically
+  Stream<QuerySnapshot> fetchMedicine() {
+    return medicineCollection.snapshots();
+  }
 
-    await documentReferencer.set(medicine.toJson()).catchError((e) {
-      if (kDebugMode) {
-        print(e);
-      }
-    });
+  Future<void> createMedicine(Medicine medicine) async {
+    try {
+      await medicineCollection.add(medicine.toJson());
+      print('Medicine added successfully.');
+    } catch (e) {
+      print('Error adding medicine: $e');
+    }
   }
 }
