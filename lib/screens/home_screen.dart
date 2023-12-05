5ca179bcb7d5ed86f0c66f9c86e7a3634766d8ba
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2023-12-05 15:10:35 +0100

    removed print

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -269,8 +269,6 @@ class _HomeScreenState extends State<HomeScreen> {
                         amount: _quantityController.text,
                       );
 
-                      print('Medicine object: ${medicine.toJson()}');
-
                       MedicationService medicationService = MedicationService();
                       await medicationService.createMedicine(medicine);
 
